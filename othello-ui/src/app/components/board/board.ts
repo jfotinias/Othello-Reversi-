6bbc1d654f58558faa2df4819cf48aa179534485
jfotinias <jfotinias@gmail.com>
2025-11-30 02:43:12 +0200

    2o frontend

--- othello-ui/src/app/components/board/board.ts
+++ othello-ui/src/app/components/board/board.ts
@@ -12,22 +12,32 @@ import { GameService } from '../../services/game.service';
 export class Board implements OnInit {
 
   board: number[][] = [];
+  boardFlat: number[] = [];
 
   constructor(private gameService: GameService) {}
+
+  getRow(index: number): number {
+  return Math.floor(index / 8);
+}
+
+getCol(index: number): number {
+  return index % 8;
+}
 
   ngOnInit(): void {
     this.loadBoard();
   }
 
-  loadBoard() {
-    this.gameService.getState().subscribe({
-      next: (state: any) => {
-        this.board = state.board;
-        console.log("BOARD LOADED:", this.board);
-      },
-      error: err => console.error("API ERROR:", err)
-    });
-  }
+loadBoard() {
+  this.gameService.getState().subscribe({
+    next: (state: any) => {
+      this.board = state.board;
+      this.boardFlat = state.board.flat();
+      console.log("BOARD LOADED:", this.boardFlat);
+    },
+    error: err => console.error("API ERROR:", err)
+  });
+}
 
   onCellClick(i: number, j: number) {
     console.log("Click:", i, j);
