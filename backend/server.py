a31baf11da572e3ffe635dff52aa6fca1a6292f3
jfotinias <jfotinias@gmail.com>
2026-07-05 16:50:36 +0300

    UI for mobile and tablets

--- backend/server.py
+++ backend/server.py
@@ -9,8 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
